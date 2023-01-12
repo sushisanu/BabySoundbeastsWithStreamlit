44509693da0aadbfea340e509eeb71d872d27253
sushisanu <sushisanuk@kkumail.com>
2023-01-12 08:22:29 +0700

    First commit
    First commit for stater this Projecct saving code.

---
+++ app.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import torch
+import matplotlib.pyplot as plt 
+from PIL import Image
+from io import *
+import tempfile
+import glob
+from datetime import datetime
+import os
+import numpy as np
+import librosa
+import librosa.display as ld
+
+
+## CFG
+cfg_model_path = 'models/SoundBeast_Model.pt' 
+
+cfg_enable_url_download = False
+if cfg_enable_url_download:
+   url = "https://archive.org/download/yoloTrained/yoloTrained.pt" #Configure this if you set cfg_enable_url_download to True
+   cfg_model_path = f"models/{url.split('/')[-1:][0]}" #config model path from url name
+## END OF CFG
+
+def imageInput(device, src):
+    
+    if src == 'Upload your own data.':
+        image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='Uploaded Image', use_column_width='always')
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
+
+            #call Model prediction--
+            model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
+            model.cuda() if device == 'cuda' else model.cpu()
+            pred = model(imgpath)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(outputpath)
+
+            #--Display predicton
+            
+            img_ = Image.open(outputpath)
+            with col2:
+                st.image(img_, caption='Model Prediction(s)', use_column_width='always')
+
+    elif src == 'From test set.': 
+        # Image selector slider
+        imgpath = glob.glob('data/images/*')
+        imgsel = st.slider('Select random images from test set.', min_value=1, max_value=len(imgpath), step=1) 
+        image_file = imgpath[imgsel-1]
+        submit = st.button("Predict!")
+        col1, col2 = st.columns(2)
+        with col1:
+            img = Image.open(image_file)
+            st.image(img, caption='Selected Image', use_column_width='always')
+        with col2:            
+            if image_file is not None and submit:
+                #call Model prediction--
+                model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
+                pred = model(image_file)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                #--Display predicton
+                    img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                    st.image(img_, caption='Model Prediction(s)')
+
+          
+
+
+
+
+
+def audioInput():
+    uploaded_audio = st.file_uploader("Upload Audio", type=["wav", "wave", "flac", "mp3", "ogg"])
+    if uploaded_audio is not None:
+        # create a temporary file
+        temp_file = tempfile.NamedTemporaryFile(suffix='.wav', delete=False)
+        temp_filename = temp_file.name
+        temp_file.write(uploaded_audio.getvalue())
+        temp_file.close()
+
+        # load audio file
+        y, sr = librosa.load(temp_filename)
+
+        # create a spectrogram from the audio
+        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)
+
+        # display the spectrogram as an image
+        plt.figure()
+        ld.specshow(librosa.power_to_db(S, ref=np.max), sr=sr, fmax=8000)
+        st.pyplot()
+        os.remove(temp_filename)
+         
+ 
+      ##  ts = datetime.timestamp(datetime.now())
+     ##   imgpath = os.path.join('data/uploads', str(ts)+uploaded_video.name)
+      ##  outputpath = os.path.join('data/video_output', os.path.basename(imgpath))
+
+       ## with open(imgpath, mode='wb') as f:
+         ##   f.write(uploaded_video.read())  # save video to disk
+
+       ## st_video = open(imgpath, 'rb')
+       ## video_bytes = st_video.read()
+       ## st.video(video_bytes)
+       ## st.write("Uploaded Video")
+      ##  detect(weights=cfg_model_path, source=imgpath, device=0) if device == 'cuda' else detect(weights=cfg_model_path, source=imgpath, device='cpu')
+       ## st_video2 = open(outputpath, 'rb')
+      ##  video_bytes2 = st_video2.read()
+       ## st.audio(video_bytes2)
+         # audio_byte.write("Model Prediction")
+
+
+def main():
+    # -- Sidebar
+    st.sidebar.title('⚙️Options')
+    datasrc = st.sidebar.radio("Select input source.", ['From test set.', 'Upload your own data.'])
+    
+        
+                
+    option = st.sidebar.radio("Select input type.", ['Image', 'Audio'])
+    if torch.cuda.is_available():
+        deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = False, index=1)
+    else:
+        deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = True, index=0)
+    # -- End of Sidebar
+
+    st.header('📦BabySoundbeast Detection')
+    st.subheader('👈🏽 Select options left-haned menu bar.')
+    st.sidebar.markdown("https://github.com/thepbordin/Obstacle-Detection-for-Blind-people-Deployment")
+    if option == "Image":    
+        imageInput(deviceoption, datasrc)
+    elif option == "Audio": 
+        audioInput()
+
+    
+
+if __name__ == '__main__':
+  
+    main()
+
+# Downlaod Model from url.    
+@st.cache
+def loadModel():
+    start_dl = time.time()
+   ## model_file = wget.download(url, out="models/")
+    finished_dl = time.time()
+    print(f"Model Downloaded, ETA:{finished_dl-start_dl}")
+##if cfg_enable_url_download:
+  ##  loadModel()
+
+
+ 
+ 
