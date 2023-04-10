fca2db5cf1a33af1d7998d260b8f0b86b6e70cea
sushisanu <sushisanuk@kkumail.com>
2023-04-10 14:30:42 +0700

    lastest update
    lastest update

--- app.py
+++ app.py
@@ -10,17 +10,27 @@ import numpy as np
 import librosa
 import librosa.display as ld
 import soundfile as sf
+import easygui
+import tkinter as tk
+from tkinter import filedialog
 
 
 
 
 ## CFG
-cfg_model_path = 'models/SoundBeast_Model.pt' 
+##cfg_model_path = 'models/SoundBeast_Model.pt' 
 ## CFG V2
-## cfg_model_path = 'models/SoundBeast_ModelV2.pt' 
+cfg_model_path = 'models/SoundBeast_ModelV2.pt' 
 
 
 ## END OF CFG
+
+def save_file_dialog(default_file_name, file_types):
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(defaultfilename=default_file_name, filetypes=file_types)
+    return file_path
+
 
 def imageInput(device, src):
     if src == 'Upload your own data.':
@@ -30,13 +40,16 @@ def imageInput(device, src):
             img = Image.open(image_file)
             with col1:
                 st.image(img, caption='Uploaded Image', use_column_width='always')
+            file_name = image_file.name
+            file_ext = os.path.splitext(file_name)[-1].lower()
             ts = datetime.timestamp(datetime.now())
-            imgpath = os.path.join('data/uploads', str(ts)+image_file.name)
+            img_name = f"{str(ts)}_{file_name}"
+            imgpath = os.path.join('data/uploads', img_name)
             outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
             with open(imgpath, mode="wb") as f:
                 f.write(image_file.getbuffer())
 
-            #call Model prediction--
+            # Call Model prediction
             model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True)
             model.cpu()
             with torch.no_grad():
@@ -45,14 +58,37 @@ def imageInput(device, src):
             for im in pred.ims:
                 im_base64 = Image.fromarray(im)
                 im_base64.save(outputpath)
-                
-            #--Display predicton
-            
+
+            # Get the number at the back of the file name before the underscore
+            num_str = file_name.split("_")[-1]
+            num_second = float(num_str.split(".")[0] + "." + num_str.split(".")[1])
+            num_second = round(num_second, 2)
+            num_minutes = num_second/60
+
+            num_bboxes = len(pred.xyxy[0])
+
+            bboxes_per_minute = round(num_bboxes / num_minutes ,2)
+
+            # Display prediction
             img_ = Image.open(outputpath)
             with col2:
                 st.image(img_, caption='Model Prediction(s)', use_column_width='always')
-            num_bboxes = len(pred.xyxy[0])
-            st.write(f'Number of bounding boxes: {num_bboxes}')
+
+            st.write(f"Baby's beasts =  {num_bboxes} times.")
+            st.write(f"Time of this audio is : {num_second} second.")
+            st.write(f"Baby's beasts : {bboxes_per_minute} per minute. ")
+            if bboxes_per_minute  < 5:
+                st.write("Alert! Abnormal case' Baby's soundbeasts less than 5 times per minute.")
+                #  result = "Baby's beasts {num_bboxes} per all time. \n Baby's soundbeasts less than 5 peaks per minute."
+
+
+            else:
+                st.write("Baby's soundbeasts is stable!")
+                #  result = "Baby's beasts {num_bboxes} per minute. \n Baby's soundbeasts is stable!"
+
+
+
+
 
     elif src == 'From test set.': 
         # Image selector slider
@@ -77,7 +113,17 @@ def imageInput(device, src):
                     img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
                     st.image(img_, caption='Model Prediction(s)')
                 num_bboxes = len(pred.xyxy[0])
-                st.write(f'Number of bounding boxes: {num_bboxes}')
+                st.write(f"Baby's beasts = {num_bboxes} times")
+                st.write(f"Baby's beasts : ... per minute. ")
+                if num_bboxes  < 5:
+                    st.write("Alert! Abnormal case' Baby's soundbeasts less than 5 times per minute.")
+                    #  result = "Baby's beasts {num_bboxes} per all time. \n Baby's soundbeasts less than 5 peaks per minute."
+
+
+                else:
+                    st.write("Baby's soundbeasts is stable!")
+                    #  result = "Baby's beasts {num_bboxes} per minute. \n Baby's soundbeasts is stable!"
+                
 
           
 
@@ -112,26 +158,33 @@ def audioInput():
         
         
         # convert the audio to wav format
-        sf.write(temp_filename, y, sr, format='wav')
+        #sf.write(temp_filename, y, sr, format='wav')
         # create a spectrogram from the audio
-        S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)
+        #S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128,fmax=8000)
 
         # create a new figure
-        fig, ax = plt.subplots()
+        #fig, ax = plt.subplots()
         # display the spectrogram as an image
-        ld.specshow(librosa.power_to_db(S, ref=np.max), sr=sr, fmax=8000, ax=ax)
-        st.pyplot(fig)
-        # Old Code  
-        #create a spectrogram from the audio
-        # D = np.abs(librosa.stft(y, n_fft=2048, hop_length=512))
-        # Convert amplitude spectrogram to Decibels-scaled spectrogram
-        #DB = librosa.amplitude_to_db(D, ref = np.max)
-        # Create the spectogram
-        #fig, ax = plt.subplots(figsize = (16, 6))
-        #im = librosa.display.specshow(DB, sr=sr, hop_length=512, x_axis='time', y_axis='log',ax=ax)
-        #plt.colorbar(im)
-        #plt.title('Decibels-scaled spectrogram', fontsize=20)
+        #ld.specshow(librosa.power_to_db(S, ref=np.max), sr=sr, fmax=8000, ax=ax)
         #st.pyplot(fig)
+        
+        # Define the desired size for the output image
+        output_size = (750, 550)
+        save_size = (1400, 1100)
+
+        # create a spectrogram from the audio
+        D = np.abs(librosa.stft(y, n_fft=2048, hop_length=512))
+        # Convert amplitude spectrogram to Decibels-scaled spectrogram
+        DB = librosa.amplitude_to_db(D, ref = np.max)
+        # Create the spectogram
+        fig, ax = plt.subplots(figsize = (16, 6))
+        im = librosa.display.specshow(DB, sr=sr, hop_length=512, x_axis='time', y_axis='log',ax=ax)
+        plt.colorbar(im)
+        plt.title('Decibels-scaled spectrogram', fontsize=20)
+        # Resize the figure to the desired output size
+        fig.set_size_inches(output_size[0]/100, output_size[1]/100)
+        fig.tight_layout()
+        st.pyplot(fig)
 
         # Save the spectrogram as a PNG file
         spectrogram_filename = "spectrogram.png"
@@ -144,32 +197,81 @@ def audioInput():
         pred.render()  # render bbox in image
         for im in pred.ims:
             im_base64 = Image.fromarray(im)
+            # Resize the image to the desired output size
+            im_base64 = im_base64.resize(output_size)
             im_base64.save(os.path.join('data/outputs', os.path.basename(spectrogram_filename)))
 
-        # Display the output image
+        # Display the output image at the desired output size
         img_ = Image.open(os.path.join('data/outputs', os.path.basename(spectrogram_filename)))
-        st.image(img_, caption='Model Prediction(s)')
+        img_resized = img_.resize(output_size)
+        st.image(img_resized, caption='Model Prediction(s)', width=output_size[0])
+        img_resized = img_.resize(save_size)
+
+        
         
         # Calculate how many bounding boxies were detected in 1 minute.
         num_bboxes = len(pred.xyxy[0])
-        duration = librosa.get_duration(y, sr)
-        bboxes_per_minute = num_bboxes / (duration / 60)
-        st.write(f'Number of bounding boxes: {num_bboxes}')
-        st.write(f'Number of bounding boxes per minute: {bboxes_per_minute:.2f}')
-        if num_bboxes / (duration / 60) < 5:
-            st.write("Baby's soundbeasts less than 5 peaks per minute.")
-        else :
-            st.write("Baby's soundbeasts is stable!")   
-       
         
-        os.remove(temp_filename)
-  
+        
 
-        if st.button('Download Spectrogram'):
-            d = datetime.now()
-            filename = "spectrogram" + d.strftime("%Y-%m-%d %H-%M-%S") + '.png'
-            plt.savefig(os.path.join('data/Spectrogram', filename))
-            st.success(f'Image saved to {filename} path is data/Spectrogram')
+        #duration second time
+        duration_second = librosa.get_duration(y, sr)
+        
+        #duration divine minute
+        duration_minute = round(librosa.get_duration(y, sr) / 60, 2)
+
+        
+
+        bboxes_per_minute = num_bboxes / (duration_minute)
+        # round to 2 decimal numbers.
+        bboxes_per_minute = round(bboxes_per_minute, 2)
+        
+        
+        
+       
+        st.write(f"Baby's beasts = {num_bboxes} times.")
+        st.write(f"Baby's beasts : {bboxes_per_minute} per minute. ")
+        if bboxes_per_minute  < 5:
+            st.write("Alert! Abnormal case' Baby's soundbeasts less than 5 times per minute.")
+          #  result = "Baby's beasts {num_bboxes} per all time. \n Baby's soundbeasts less than 5 peaks per minute."
+        
+
+        else:
+            st.write("Baby's soundbeasts is stable!")
+          #  result = "Baby's beasts {num_bboxes} per minute. \n Baby's soundbeasts is stable!"
+
+        os.remove(temp_filename)
+        
+        # Add text input for username
+        username = st.text_input("Enter your name before download files:")
+        if not username:
+            st.warning("Please enter your name to download provide data")
+        else:
+            # Download Spectrogram image file
+            if st.button('Download Spectrogram image file'):
+                d = datetime.now()
+                filename = str(username) + d.strftime("-Date %d-%m-%Y Time-%H-%M-%S Duration_") + str(duration_second) + '.png'
+                folder_path = easygui.diropenbox(title="Select directory to save file")
+                if folder_path is not None:
+                    plt.savefig(os.path.join(folder_path, filename))
+                    st.success(f'"{filename}" is file name save to {folder_path}')
+
+            # Download results with text files format
+            if st.button('Download Result in text file'):
+                d = datetime.now()
+                filename = str(username) + d.strftime("_%d-%m-%Y_Time-%H-%M-%S") + ".txt"
+                with open(filename, "w") as f:
+                    f.write(f"Baby's beasts = {num_bboxes} times.\n")
+                    f.write(f"Baby's beasts : {bboxes_per_minute:.2f} per minute.\n")
+                    if bboxes_per_minute < 5:
+                        f.write("Alert! Abnormal case' Baby's soundbeasts less than 5 times per minute.")
+                    else:
+                        f.write("Baby's soundbeasts is stable!\n")
+                with open(filename, "r") as f:
+                    file_contents = f.read()
+                st.download_button(label="Download", data=file_contents, file_name=filename, mime="text/plain")
+
+
 
 
 
@@ -186,7 +288,18 @@ def audioInput():
 
 def main():
     # -- Sidebar
-    st.sidebar.title('⚙️Options')
+    st.markdown(
+        f"""
+        <style>
+            .sidebar .sidebar-content {{
+                background-color: #1E90FF;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.sidebar.title('BabySoundbeast Application')
     option = st.sidebar.radio("Select input type.", ['Image', 'Audio'])
     if option == 'Audio':
         datasrc = st.sidebar.radio("Select input source.", ['From test set.', 'Upload your own data.'], disabled = True, index=1)
@@ -199,13 +312,26 @@ def main():
         deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = True, index=0)
     # -- End of Sidebar
 
-    st.header('❤️BabySoundbeast Detection')
-    st.subheader('👈🏽 Select options left-haned menu bar.')
-    st.sidebar.markdown("Visit my github: https://github.com/sushisanu/BabySoundbeastsWithStreamlit")
+    st.header(chr(0x1F6BC) + 'BabySoundbeast Detection')
+    st.subheader('Select options left-haned menu bar.')
+    st.sidebar.markdown("-----")
+    st.sidebar.markdown("Develope By Mr.sushisanu kulprom")
+    st.sidebar.markdown("supervisor : Prof. Wanida Kanarkard ")
+    st.sidebar.markdown("co-supervisors :")
+    st.sidebar.markdown("1.Mr. Nawapak Eua-anant")
+    st.sidebar.markdown("2.Mr. Wasu Chaopanon")
+    st.sidebar.markdown("Project leader: Dr.thiwawan thepha")
+    st.sidebar.markdown("-----")
+    
+    st.sidebar.markdown("This project is 4th year project of the Faculty of Computer Engineering. Khon Kaen University, Academic Year 2022")
+
     if option == "Image":    
         imageInput(deviceoption, datasrc)
     elif option == "Audio": 
         audioInput()
+
+
+
 
     
 
